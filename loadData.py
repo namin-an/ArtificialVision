e327e2fd7026e8f1a04e96a09274a5bbbfc58a9d
namin-an <namin0202@gmail.com>
2023-02-15 20:44:07 +0900

    Change par name

--- loadData.py
+++ loadData.py
@@ -6,21 +6,16 @@ import numpy as np
 import matplotlib.pyplot as plt 
 
 
-def loadData(com_list_n, which_data_mode, train_path, test_path, ft_path, is_finetune, test_type, accessory_list, light_list, expression_list, camera_list, seed, r, model_type2):
-
-    if which_data_mode == 'train':
-        path = train_path
-    elif which_data_mode == 'test':
-        path = test_path
+def loadData(com_list_n, train_test, path, ft_path, is_finetune, data_type, accessory_list, light_list, expression_list, camera_list, seed, r, model_type2):
 
     file_path_list, label_list = list(), list()
     start_time = time.time()
 
-    if is_finetune == 'ft' and which_data_mode == 'train':
+    if is_finetune == 'ft' and train_test == 'train':
         tot_num_trains = len(os.listdir(os.path.join(path, str(com_list_n[0]))))
         zeros_and_ones = [0]*500 + [1]*500 + [2]*(tot_num_trains-1000) 
         random.seed(random)
-        rand_indexes = random.shuffle(zeros_and_ones)
+        random.shuffle(zeros_and_ones)
         
         filePaths = []
         for i in range(1000): 
@@ -28,7 +23,7 @@ def loadData(com_list_n, which_data_mode, train_path, test_path, ft_path, is_fin
             if ran_val == 0: # high-resolution
                 for face in com_list_n:
                     one_folder = os.path.join(path, str(face))
-                    file_name = os.listdir(one_folder)[i] # same index i for all files of faces
+                    file_name = os.listdir(one_folder)[i] 
                     
                     sl_split = file_name.split('L')
                     file_name2 = sl_split[-1]
@@ -42,7 +37,7 @@ def loadData(com_list_n, which_data_mode, train_path, test_path, ft_path, is_fin
                         file_path_list.append(os.path.join(one_folder, file_name))
                         label_list.append(str(face))
 
-            elif ran_val == 1: 
+            elif ran_val == 1: # fine-tune
                 for face in com_list_n:
                     one_folder = os.path.join(ft_path, str(face))
                     file_name = os.listdir(one_folder)[i]
@@ -55,22 +50,22 @@ def loadData(com_list_n, which_data_mode, train_path, test_path, ft_path, is_fin
                     file_name4 = c_split[-1]
                     dot_split = file_name4.split('.')
                     
-                    if (str(sl_split[0][-1]) not in accessory_list) or (str(e_split[0]) not in light_list) or (str(c_split[0][-1]) not in expression_list) or (str(dot_split[0]) not in camera_list):                       
+                    if (str(sl_split[0][-1]) in accessory_list) and (str(e_split[0]) in light_list) and (str(c_split[0][-1]) in expression_list) and (str(dot_split[0]) in camera_list):                     
                         file_path_list.append(os.path.join(one_folder, file_name))
                         label_list.append(str(face))
                 
     else:
         folderPaths = [os.path.join(path, str(folder_name)) for folder_name in com_list_n]
-        for folderPath in folderPaths: # for all participants
+        for folderPath in folderPaths:
             
-            folder_name = os.path.basename(folderPath) # real person id (com_list_n[i], i=0,1,...,r)
+            folder_name = os.path.basename(folderPath) 
             filePaths = [os.path.join(folderPath, file_name) for file_name in os.listdir(folderPath)]
 
-            if which_data_mode == 'train':
+            if train_test == 'train':
                 random.seed(seed)
-                train_int_list = random.sample(range(len(filePaths)), 1000) # ~ 1K images from over 4,000 images (same for all ppl (folders))
+                train_int_list = random.sample(range(len(filePaths)), 1000) # ~ 1K images from over 4k images (same for all facial classes)
 
-            for i, filePath in enumerate(filePaths): # for all files per participant
+            for i, filePath in enumerate(filePaths): 
                 file_name = os.path.basename(filePath)
             
                 sl_split = file_name.split('L')
@@ -81,21 +76,21 @@ def loadData(com_list_n, which_data_mode, train_path, test_path, ft_path, is_fin
                 file_name4 = c_split[-1]
                 dot_split = file_name4.split('.')
 
-                if which_data_mode == 'train': 
-                    if i in train_int_list: # take only 1,000 images from 4,872 images
-                        if (str(sl_split[0][-1]) not in accessory_list) or (str(e_split[0]) not in light_list) or (str(c_split[0][-1]) not in expression_list) or (str(dot_split[0]) not in camera_list):                       
+                if train_test == 'train': 
+                    if i in train_int_list: # 1k images from 4,872 images
+                        if (str(sl_split[0][-1]) in accessory_list) and (str(e_split[0]) in light_list) and (str(c_split[0][-1]) in expression_list) and (str(dot_split[0]) in camera_list):
                             file_path_list.append(filePath)
                             label_list.append(folder_name)
 
-                elif which_data_mode == 'test':
+                elif train_test == 'test':
                     if (str(sl_split[0][-1]) in accessory_list) and (str(e_split[0]) in light_list) and (str(c_split[0][-1]) in expression_list) and (str(dot_split[0]) in camera_list):
                         file_path_list.append(filePath)
                         label_list.append(folder_name)
 
-    if which_data_mode == 'train': 
+    if train_test == 'train': 
         assert len(file_path_list) <= 1000*r 
-    elif which_data_mode == 'test':
-        if test_type == 'normal':
+    elif train_test == 'test':
+        if data_type == 'normal':
             assert len(file_path_list) == 9*r 
 
 
@@ -116,27 +111,17 @@ def loadData(com_list_n, which_data_mode, train_path, test_path, ft_path, is_fin
     old_uniq_labels = list(set(y))
     key_list = list(range(r))
     dictionary = dict(zip(old_uniq_labels, key_list))
+
     for i, face_num in enumerate(y):
         y[i] = dictionary[face_num]
+
     new_uniq_labels = list(set(y))
-    assert len(old_uniq_labels) == r
-    assert len(new_uniq_labels) == r
-    print(f'Old unique labels: {old_uniq_labels}')
-    print(f'New unique labels: {new_uniq_labels}')
     y = np.array(y)
 
+    assert len(old_uniq_labels) == r
+    assert len(new_uniq_labels) == r
+
+    print(f'Old unique labels: {old_uniq_labels}')
+    print(f'New unique labels: {new_uniq_labels}')
+
     return X, y, file_path_list, old_uniq_labels, new_uniq_labels
-
-
-def visualizeData(Xtrain, ytrain, Xtest, ytest):
-    random_idx = random.randint(0, len(Xtrain))
-    plt.imshow(Xtrain[random_idx], cmap='gray')
-    plt.title(f"Training example #{random_idx} (Class: {ytrain[random_idx]})")
-    plt.axis('off')
-    plt.show()
-
-    random_idx = random.randint(0, len(Xtest))
-    plt.imshow(Xtest[random_idx], cmap='gray')
-    plt.title(f"Testing example #{random_idx} (Class: {ytest[random_idx]})")
-    plt.axis('off')
-    plt.show()
